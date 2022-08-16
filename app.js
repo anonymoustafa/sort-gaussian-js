fb0fec46db7e3db0335b019e8a15fd60e571eff0
anonymoustafa <anonymoustafa@outlook.com>
2022-08-16 15:54:28 +0430

    adding js files to the remote repo

---
+++ app.js
@@ -0,0 +1,10 @@
+const input    =  [350,-8,19,34,10,0.6,60,-30,-30,-12,123,2.34]
+let side       = true;
+const inputSorted = input.sort(function(a, b){return a - b});
+
+inputSorted.forEach(element => {
+  
+});
+
+console.log(inputSorted);
+
---
+++ app1.js
@@ -0,0 +1,4 @@
+f = [1,2,3]
+while (f.pop) {
+    console.log("it won't work!");
+}
