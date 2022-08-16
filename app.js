0aa7128095105edd3a0457f5c6f3f244635b7438
anonymoustafa <anonymoustafa@outlook.com>
2022-08-16 16:21:15 +0430

    phasw one completed

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const input    =  [350,-8,19,34,10,0.6,60,-30,-30,-12,123,2.34]
-let side       = true;
+const input = [350,-8,19,34,10,0.6,60,-30,-30,-12,123,52,2.34,7,-44,-55];
+const output = [];
+
 const inputSorted = input.sort(function(a, b){return a - b});
 
-inputSorted.forEach(element => {
+for (let index = inputSorted.length-1; index >= 0; index--) {
+  if (index%2==0 && !isNaN(inputSorted[index])) {
+    output.push(inputSorted[index])
+  } else {
+    output.unshift(inputSorted[index])
+  }
   
-});
+}
 
-console.log(inputSorted);
+console.log(`array has \n ${input.length} elements \n sorted min to max has \n ${inputSorted.length} elements \n output has \n ${output.length} elements`)
 
+console.log(output);
